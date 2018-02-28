836aa08a915b54a34b14c83dc7ce562795e81450
zhangxu <zx@reedo.cn>
2018-02-28 22:57:48 +0800

    add blog detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
-
+app_name = 'blog'
 urlpatterns = [
-    url('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('post/<int:pk>/', views.detail, name='detail'),
 ]
 
