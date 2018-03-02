abe3a8f41ae8fa48c6bd8250b7d80cf585ac2b0e
zhangxu <zx@reedo.cn>
2018-03-02 23:17:12 +0800

    add side content

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 
@@ -9,6 +9,15 @@ from .models import Post
 def index(request):
     # return HttpResponse('hello blog')
     post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', {'post_list':post_list})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month).order_by('-created_time')
+    return render(request, 'blog/index.html', {'post_list':post_list})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
     return render(request, 'blog/index.html', {'post_list':post_list})
 
 
