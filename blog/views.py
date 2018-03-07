d3d2d1f70bbfb23177b86c48ed6ec1c0930ee369
zhangxu <zx@reedo.cn>
2018-03-07 22:17:29 +0800

    add pagination for index

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,50 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     contex_object_name = 'post_list'
+    paginate_by =1
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
+    
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+        right = page_range[page_number:page_number + 2]
+        left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+        if right:
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        if left:
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        data = {
+            'left':left,
+            'right':right,
+            'left_has_more':left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last':last,
+        }
+        return data
+
+
 
 
 # 改用下面类方法实现
