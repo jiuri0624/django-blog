06871be49ad21eba00fd1e2b896c39fa4e6e0c5b
zhangxu <zx@reedo.cn>
2018-03-09 22:11:54 +0800

    add post counts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
-from ..models import Post, Category
+from django.db.models.aggregates import Count
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -15,4 +16,4 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
